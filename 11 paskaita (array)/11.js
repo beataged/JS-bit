6fffa176830ff04f8bace708bbcae607624fd7f9
Beata Gedgaudaite <beatagedgaudaite@gmail.com>
2021-08-01 22:06:57 +0300

    08/01

--- 11 paskaita (array)/11.js
+++ 11 paskaita (array)/11.js
@@ -189,20 +189,147 @@ console.log('reiksmes didesnes uz 2: ', mas8_2);
 //Sukurti masyvą (piniginę su dviem skyreliais) iš dviejų elementų, kurio pirmas elementas būtų masyvas iš 8 uždavinio su monetom, o antras elementas masyvas iš 8 uždavinio su popieriniais pinigais;
 console.log("_______________9_____________________");
 
-let mas8 = [];
-let mas8_1 = [];
-let mas8_2 = [];
+let mas9 = [];
+let mas9_1 = [];
+let mas9_2 = [];
 
 for (let i = 0; i < rand(10, 30); i++) {
-    mas8.push(rand(0, 10));
-    if(mas8[i] <= 2){
-        mas8_1.push(mas8[i]);
+    mas9.push(rand(0, 10));
+    if(mas9[i] <= 2){
+        mas9_1.push(mas9[i]);
     }else{
-        mas8_2.push(mas8[i]);
+        mas9_2.push(mas9[i]);
     }
 
 }
-console.log(mas8); 
 
-console.log('reiksmes mazesnes arba lygios 2: ', mas8_1);
-console.log('reiksmes didesnes uz 2: ', mas8_2);
+console.log(mas9); 
+
+let pinigine = [mas9_1, mas9_2];
+
+console.log(pinigine);
+
+//Į 9 uždavinio masyvą, piniginę su dviem skyreliais, pridėti trečią skyrelį- masyvą su kortelėm: ['KIKA', 'Euro Vaistinė', 'Drogas', 'Ačiū', 'Lietuvos Geležinkeliai', 'Mano RIMI'];
+console.log("_______________10_____________________");
+
+let korteles = ['KIKA', 'Euro Vaistinė', 'Drogas', 'Ačiū', 'Lietuvos Geležinkeliai', 'Mano RIMI'];
+
+let pinigine2 = [mas9_1, mas9_2, korteles];
+
+console.log(pinigine2);
+
+//Korteles skyrelyje sudėlioti (išrūšiuoti) pagal abėcėlę;
+console.log("_______________11_____________________");
+
+console.log(pinigine2[2].sort());
+
+
+//Į kortelių skyrelį pridėti mokėjimo kortelių 'MasterCard', 'Visa' (su rand generuokite atsitiktines reikšmes 'MasterCard' arba 'Visa' ir rašykite į masyvą) iš skirtingų bankų tiek, kad skyrelis (masyvo ilgis) pasidarytų lygus 20;
+console.log("_______________12_____________________");
+
+while(pinigine2[2].length < 20){
+    let bankoKortele;
+    rand(1,2) == 1 ? bankoKortele = 'MasterCard' : bankoKortele = "Visa";
+    pinigine2[2].push(bankoKortele);
+}
+
+console.log(pinigine2);
+
+//Paskaičiuokite, kokio tipo kortelių ('MasterCard' arba 'Visa') yra daugiau;
+console.log("_______________13_____________________");
+
+let daugiauMaster = 0;
+let daugiauVisa = 0;
+let ilgis = pinigine2[2].length
+
+for (let i = 0; i < ilgis; i++) {
+    if(pinigine2[2][i] == 'MasterCard'){
+        daugiauMaster++;
+    }
+    if(pinigine2[2][i] == 'Visa'){
+        daugiauVisa++;
+    }
+}
+if(daugiauMaster > daugiauVisa){
+    console.log('Daugiau MasterCard');
+}else if (daugiauMaster < daugiauVisa){
+    console.log('Daugiau Visa');
+}else{
+    console.log('korteliu po lygiai');
+}
+console.log(daugiauMaster, daugiauVisa);
+
+
+const counts = {};
+pinigine2[2].forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+
+if(counts['MasterCard'] > counts['Visa']){
+    console.log('Daugiau MasterCard');
+}else if (counts['MasterCard'] < counts['Visa']){
+    console.log('Daugiau Visa');
+}else{
+    console.log('korteliu po lygiai');
+}
+
+console.log(counts);
+
+//Sukurkite masyve (piniginėje) ketvirtą elementą (skyrelį) į kurį įdėkite 10 loterijos bilietų, kurių numerius sugeneruokite atsitiktinai su rand funkcija nuo 1000000000 iki 9999999999;
+console.log("_______________14_____________________");
+
+pinigine2.push([]);
+
+for(let i = 0; i < 10; i++){
+    let bilietai = rand(1000000000, 9999999999);
+    pinigine2[3].push(bilietai);
+}
+console.log(pinigine2);
+
+//Loterijos bilietų masyvą išrūšiuoti nuo didžiausio numerio iki mažiausio;
+console.log("_______________15_____________________");
+
+pinigine2[3].sort((a, b) => (a > b ? -1 : 1))
+
+console.log(pinigine2[3]);
+
+//Į piniginės popierinių pinigų skyrelį įdėti 500 pinigų mažom kupiūrom ( generuoti atsitiktinius skaičius nuo 3 iki 10 ir dėti kaip naujus elementus, kol įdėta suma bus lygi 500);
+console.log("_______________16_____________________");
+
+let sum = 0;
+
+while (sum < 500){
+    pinigine2[1].push(rand(3,10));
+    sum = 0;
+    pinigine2[1].forEach(item => sum += item)
+
+}
+
+console.log(pinigine2[1], 'suma', sum);
+
+//Patikrinti ar ką nors laimėjote. Bilieto numerius dalinkite iš 777 ir jeigu numeris išsidalins be liekanos - jūs laimėjote! Suskaičiuokite, kiek buvo laimingų bilietų.
+console.log("_______________17_____________________");
+
+const loterija = pinigine2[3].map(i => i%777);
+
+const laimejimai = loterija.filter(i => i==0).length;
+
+console.log(loterija, laimejimai);
+
+let counter = 0;
+for(let i = 0; i < pinigine2[3].length; i++){
+    if(pinigine2[3][i]%777 == 0){
+        counter++;
+    }
+}
+
+console.log('Laimingu bilietu',counter);
+
+//Sukurkite penktą skyrelį ir į jį sudėkite nuotraukas: ['šuo', 'katė', 'automobilis', 'namas', 'kiemas'] ir jas išrūšiuokite pagal žodžių ilgį taip, kad pirma eitų trumpiausi žodžiai;
+console.log("_______________18_____________________");
+
+let nuotraukos =  ['šuo', 'katė', 'automobilis', 'namas', 'kiemas'];
+
+pinigine2.push(nuotraukos);
+
+pinigine2[4].sort((a, b) => (a.length > b.length ? 1 : -1))
+
+console.log(pinigine2);
