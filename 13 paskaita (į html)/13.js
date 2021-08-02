fc828aa1091bbcf0d70aa7e2201c506b2e4a6ce9
Beata Gedgaudaite <beatagedgaudaite@gmail.com>
2021-08-02 21:27:41 +0300

    08/02

---
+++ 13 paskaita (į html)/13.js
@@ -0,0 +1,90 @@
+let masyvas = [3, 2, 5, 1, 4];
+
+console.log(masyvas.slice());
+
+function lygintuvas(a,b){
+    console.log('a:', a, 'b: ', b);
+    return a - b;
+}
+
+masyvas.sort(lygintuvas);
+
+console.log(masyvas);
+
+let ezerai = [
+    [2, 'Didelis'],
+    [8, 'Ilgas'],
+    [11, 'Trumpas'],
+    [1, 'Apvalus'],
+    [7, 'Gilus']
+];
+
+function lygintuvas2(a, b) {
+
+    console.log('a:', a, 'b: ', b);
+
+    if (a.length > b.length) {
+        return 1;
+    } else if (a.length < b.length) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+function ezeruLygintuvas(a, b) {
+
+    console.log('a:', a, 'b: ', b);
+
+    if (a[1] > b[1]) {
+        return 1;
+    } else if (a[1] < b[1]) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+console.log(ezerai.sort(ezeruLygintuvas));
+
+ezerai.sort(lygintuvas2);
+
+console.log(ezerai);
+console.log("____________________________")
+function zodzioIlgis(a, b) {
+
+    console.log('a:', a, 'b: ', b);
+
+    if (a[1].length > b[1].length) {
+        return -1;
+    } else if (a[1].length < b[1].length) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+console.log(ezerai.sort(zodzioIlgis));
+
+console.log('____________sujungimas su HTML___________');
+
+let newH1 = document.createElement('h1');
+let newH12 = document.createElement("h1");
+
+let oldH1 = document.querySelector(".old");
+let body = document.querySelector('body');
+let container = document.querySelector('container');
+
+// container.insertBefore(newH1, oldH1);
+// container.appendChild(newH1);
+
+console.log(newH1);
+console.log(oldH1);
+console.log(body);
+
+// let hello = document.createElement('Hello');
+// let hello = document.createTextNode('Hello');
+
+for(let i = 0; i < 10; i++){
+    let hello = document.createTextNode('Hello');
+}
