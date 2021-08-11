2f7745bbb11ffcc9e83a27d3a923dd06675d772f
Beata Gedgaudaite <beatagedgaudaite@gmail.com>
2021-08-11 23:57:37 +0300

    08/11

---
+++ 20 paskaita (objektai)/20.js
@@ -0,0 +1,16 @@
+const obj2 = {
+    // savybe1: 'zaluma',
+    // savybe2: 'geltonuma',
+    // savybe3: 42,
+    // savybe4: [1, 5, 6],
+    metodas1: function() {
+        console.log('as metodas1')
+    },
+    metodas2: function() {
+        console.log('as metodas2')
+    },
+    metodas3: function() {
+        console.log('as metodas3')
+    }
+};
+
