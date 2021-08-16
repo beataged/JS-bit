70331e0b881abc9d3ade2b77bea1f4d86b5d001a
Beata Gedgaudaite <beatagedgaudaite@gmail.com>
2021-08-16 21:27:30 +0300

    08/16

--- 23 paskaita/23.js
+++ 23 paskaita/23.js
@@ -5,19 +5,28 @@ class Kibiras1 {
     static visiAkmenys = 0;
 
     static akmenuSkaiciusVisuoseKibiruose(){
+        console.log("Visuose kibiruose yra akmenu: ", this.visiAkmenys)
+    };
 
-    }
+    static bendrasAkmenuSkaicius(akmenuSkaicius){
+        this.visiAkmenys += akmenuSkaicius;
+    };
 
     constructor() {
         this.akmenuKiekis = 0;
     };
+
     prideti1Akmeni(){
         this.akmenuKiekis ++; //i objekta, konkretus kibiras
-        Kibiras1.visiAkmenys++;//i bendra statine savybe skaiciuoja, visiems kibirams 
+        this.constructor.bendrasAkmenuSkaicius(1);//i bendra statine savybe skaiciuoja, visiems kibirams. this parodo i klase, o construkctor parodo is kur objektas kiles (this.constructor === Kibiras1)
+        //sitas objektas (this) yra konstruojamas (constructor) ===> grazina klase is ko sitas objektas padarytas
     };
+
     pridetiDaugAkmenu (kiekis){
         this.akmenuKiekis += kiekis;
+        this.constructor.bendrasAkmenuSkaicius(kiekis)
     };
+
     kiekPririnktaAkmenu() {
         console.log('Kibire yra akmenu: ',this.akmenuKiekis);
     };
@@ -38,3 +47,64 @@ kibiras66.kiekPririnktaAkmenu();
 kibiras77.prideti1Akmeni();
 kibiras77.pridetiDaugAkmenu(18);
 kibiras77.kiekPririnktaAkmenu();
+
+Kibiras1.akmenuSkaiciusVisuoseKibiruose();
+
+//4. (STATIC) Sukurti metodą keleiviuSkaiciusVisuoseTroleibusuose(), kuris rodytų bendrą keleivių skaičių visuose Troleibusas objektuose. Bendram kelevių skaičiaus skaičiavimui sukurkite statinį metodą bendrasKeleiviuSkaicius(keleiviuSkaicius), kuris pridėtų arba atimtų keleivius iš statinės savybės visiKeleiviai (kurioje yra įrašytas bendras keleivių skaičius). Taip pat atitinkamai modifikuokite metodus ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius).
+
+console.log('______________Troleibusas2____________');
+
+class Troleibusas{
+
+    static visiKeleiviai = 0;
+    
+    static keleiviuSkaiciusVisuoseTroleibusuose(){
+        console.log("Visuose troleibusuose yra keleiviu: ", this.visiKeleiviai);
+    };
+
+    static bendrasKeleiviuSkaicius(keleiviuSkaicius){
+        this.visiKeleiviai += keleiviuSkaicius;
+    };
+
+
+    constructor(){
+        this.keleiviuSkaicius = 0;
+    };
+    ilipa(keleiviuSkaicius){
+        this.keleiviuSkaicius += keleiviuSkaicius; 
+        this.constructor.bendrasKeleiviuSkaicius(keleiviuSkaicius);
+    };
+    islipa(keleiviuSkaicius){
+        if(this.keleiviuSkaicius < keleiviuSkaicius){
+            keleiviuSkaicius = this.keleiviuSkaicius;
+        }
+        
+        this.keleiviuSkaicius -= keleiviuSkaicius;
+        this.constructor.bendrasKeleiviuSkaicius(-keleiviuSkaicius);
+    };
+    vaziuoja(){
+        if(this.keleiviuSkaicius > 0){
+        console.log('Vaziuoja keleiviu', this.keleiviuSkaicius);
+        }else {
+            console.log('Vaziuoja keleiviu: 0')
+        }
+    };
+}
+
+const troleibusas = new Troleibusas();
+const troleibusas2 = new Troleibusas();
+const troleibusas3 = new Troleibusas();
+
+troleibusas.ilipa(500);
+troleibusas.islipa(100);
+troleibusas.vaziuoja();
+
+troleibusas2.ilipa(100);
+troleibusas2.islipa(50);
+troleibusas2.vaziuoja();
+
+troleibusas3.ilipa(300);
+troleibusas3.islipa(200);
+troleibusas3.vaziuoja();
+
+Troleibusas.keleiviuSkaiciusVisuoseTroleibusuose();
--- Projektai/zoo.js
+++ Projektai/zoo.js
@@ -1,26 +1,56 @@
+//sukurti objektiną metoda render() ir į jį iš konstruktoriaus perkelti this.createAnimalElement(); this.createAnimalHtml();
+
+
 class Animal {
+
+    //2. Sukurti statini metodą createAnimal su keturiais argumentais, kuris gamintų Animal objektą ir dėtų jį į statinę savybę animals
+    static mas = [];
+
+    static createAnimal(specie, tailLong, color, hasHorn) {
+        this.mas.push(new Animal(specie, tailLong, color, hasHorn));
+    };
+
+    //4 Sukurti statinį metodą renderZoo, kuris atvaizduotų visus sukurtus gyvūnus iš statinės savtbės-masyvo animal
+
+    static renderZoo() {
+        this.mas.forEach(animal => {
+            animal.render();
+        });
+    };
+
+    //5 Sukurti statinį metodą clearZoo() kuris ištrintų visus žvėris iš DOM HTML, bet ne iš masyvo animals
+
+    static clearZoo(){
+        this.mas.forEach(animal => document.querySelector('body').removeChild(animal.element));
+    };
+
     constructor(specie, tailLong, color, hasHorn) {
         this.specie = specie,
         this.tailLong = tailLong,
         this.color = color,
         this.hasHorn = hasHorn;
-        console.log(this.specie);
+    };
+
+    render() {
         this.createAnimalElement();
         this.createAnimalHtml();
-    };
-//5. Sukurkite createAnimalElement metodą, kuris sukuria naują DIV elementą ir jį įdeda į DOM'ą (htmlą)
-//6. Nuorodą į tą elementą užsaugokite naujai sukurtoje savybėje element
-//7. createAnimalElement įdėkite į konstruktorių ir paleiskite jame
-    createAnimalElement(){
+    }
+
+    //5. Sukurkite createAnimalElement metodą, kuris sukuria naują DIV elementą ir jį įdeda į DOM'ą (htmlą)
+    //6. Nuorodą į tą elementą užsaugokite naujai sukurtoje savybėje element
+    //7. createAnimalElement įdėkite į konstruktorių ir paleiskite jame
+
+    createAnimalElement() {
         this.element = document.createElement('div');
         document.body.appendChild(this.element);
 
     };
 
-//8. sukurti metodą animalHtml, kuriame sudėliojame animalo apraš htmlą
-//9. aprašą įdėti su innerHTML į sukurtą savybę element su div tagu
-//10. paleisti padarytą metodą konstruktoriuje
-    createAnimalHtml(){
+    //8. sukurti metodą animalHtml, kuriame sudėliojame animalo apraš htmlą
+    //9. aprašą įdėti su innerHTML į sukurtą savybę element su div tagu
+    //10. paleisti padarytą metodą konstruktoriuje
+
+    createAnimalHtml() {
         const horn = this.hasHorn ? 'Has horn' : 'No horn';
         const html = `
         <h1> ${this.specie}</h1>
@@ -34,23 +64,22 @@ class Animal {
 
 };
 
-let zveriuMasyvas = [];
+Animal.createAnimal('Zebras', 36, 'black-white', false);
+Animal.createAnimal('Zirafa', 30, 'brown-white', true);
+Animal.createAnimal('Liutas', 35, 'brown', false);
+Animal.createAnimal('Dramblys', 20, 'grey', false);
 
-const gyvunas1 = new Animal('Zebras', 36, 'black-white', false);
-const gyvunas2 = new Animal('Zirafa', 30, 'brown-white', true);
-const gyvunas3 = new Animal('Liutas', 35, 'brown', false);
-const gyvunas4 = new Animal('Dramblys', 20, 'grey', false);
+console.log(Animal.mas);
 
-zveriuMasyvas.push(gyvunas1, gyvunas2, gyvunas3, gyvunas4);
-
-console.log(zveriuMasyvas);
+Animal.renderZoo();
+Animal.clearZoo();
 
 const specie = document.querySelector('#specie');
 const tale = document.querySelector('#tail');
 const color = document.querySelector('#color');
 
 document.querySelector('#button').addEventListener('click', () => {
-    zveriuMasyvas.push(new Animal(specie.value, tail.value, color.value, hasHorns.checked ));
+    Animal.mas.push(Animal.createAnimal(specie.value, tail.value, color.value, hasHorns.checked));
 })
 
 //14. sukurti metodą kuris generuotų atsitiktinius skaičius nuo 1000000 iki 9999999 ir priskirti tą skaičių savybei id, ir pasileistų konstruktoriuje
