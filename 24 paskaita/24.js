5b5208e74e1b61b1a968af7d4cb059594cdbdd0c
Beata Gedgaudaite <beatagedgaudaite@gmail.com>
2021-08-17 21:31:34 +0300

    08/17

---
+++ 24 paskaita/24.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector('h1');
+
+console.log(h1);
+
+h1.dataset.darDaugiau = 103;
+
+console.log(h1.dataset);
+
+//dataset leidzia saugot papildoma info tame elemente.
--- Projektai/zoo.js
+++ Projektai/zoo.js
@@ -6,6 +6,8 @@ class Animal {
     //2. Sukurti statini metodą createAnimal su keturiais argumentais, kuris gamintų Animal objektą ir dėtų jį į statinę savybę animals
     static mas = [];
 
+    element; //nuoroda i html taga su animalu
+
     static createAnimal(specie, tailLong, color, hasHorn) {
         this.mas.push(new Animal(specie, tailLong, color, hasHorn));
     };
@@ -20,20 +22,84 @@ class Animal {
 
     //5 Sukurti statinį metodą clearZoo() kuris ištrintų visus žvėris iš DOM HTML, bet ne iš masyvo animals
 
-    static clearZoo(){
+    static clearZoo() {
         this.mas.forEach(animal => document.querySelector('body').removeChild(animal.element));
     };
 
+    static buttonCreate() {
+        const specie = document.querySelector('#specie');
+        const tale = document.querySelector('#tail');
+        const color = document.querySelector('#color');
+
+        document.querySelector('#button').addEventListener('click', () => {
+            this.clearZoo();
+            this.createAnimal(specie.value, tail.value, color.value, hasHorns.checked);
+            this.renderZoo();
+        });
+    };
+
+    static start() {
+        this.buttonCreate();
+
+        // Animal.createAnimal('Zebras', 36, 'black-white', false);
+        // Animal.createAnimal('Zirafa', 30, 'brown-white', true);
+        // Animal.createAnimal('Liutas', 35, 'brown', false);
+        // Animal.createAnimal('Dramblys', 20, 'grey', false);
+
+        this.load();
+
+        Animal.renderZoo();
+    };
+
+    static deleteAnimal(id) {
+        this.mas.forEach((animal, index) => {
+            if (id == animal.id) {
+                this.clearZoo();
+                this.mas.splice(index, 1);
+                this.renderZoo();
+            }
+
+        });
+        this.save();
+    };
+
+
+
+    static save() {
+        const data = [];
+        this.mas.forEach(animal => {
+            data.push({
+                specie: animal.specie,
+                tailLong: animal.tailLong,
+                color: animal.color,
+                hasHorn: animal.hasHorn
+            });
+        });
+        localStorage.setItem('zooApp', JSON.stringify(data));
+
+
+        console.log(data);
+
+    };
+
+    static load() {
+        if (null === localStorage.getItem('zooApp')) {localStorage.setItem('zooApp', JSON.stringify([]));}
+
+        JSON.parse(localStorage.getItem('zooApp')).forEach(animal => this.createAnimal(animal.specie, animal.tailLong, animal.color, animal.hasHorn));
+    }
+
     constructor(specie, tailLong, color, hasHorn) {
         this.specie = specie,
-        this.tailLong = tailLong,
-        this.color = color,
-        this.hasHorn = hasHorn;
+            this.tailLong = tailLong,
+            this.color = color,
+            this.hasHorn = hasHorn;
+        this.createAnimalRandom();
     };
 
     render() {
         this.createAnimalElement();
         this.createAnimalHtml();
+        this.deleteButton();
     }
 
     //5. Sukurkite createAnimalElement metodą, kuris sukuria naują DIV elementą ir jį įdeda į DOM'ą (htmlą)
@@ -57,31 +123,24 @@ class Animal {
         <span> Tail long: ${this.tailLong}cm</span>
         <span> Color: ${this.color}</span>
         <i> ${horn}</i>
-        <button data-id="${this.id}">Delete</button>
+        <button class = 'delete' >Delete</button>
+        <button class= 'edit'>Edit</button>
         `;
+
         this.element.innerHTML = html;
     }
 
+    createAnimalRandom() {
+        this.id = Math.floor(Math.random() * 9000000) + 1000000;
+    };
+
+    deleteButton() {
+        this.element.querySelector('.delete').addEventListener('click', () => this.constructor.deleteAnimal(this.id));
+    };
+
 };
 
-Animal.createAnimal('Zebras', 36, 'black-white', false);
-Animal.createAnimal('Zirafa', 30, 'brown-white', true);
-Animal.createAnimal('Liutas', 35, 'brown', false);
-Animal.createAnimal('Dramblys', 20, 'grey', false);
 
 console.log(Animal.mas);
 
-Animal.renderZoo();
-//Animal.clearZoo();
-
-const specie = document.querySelector('#specie');
-const tale = document.querySelector('#tail');
-const color = document.querySelector('#color');
-
-document.querySelector('#button').addEventListener('click', () => {
-    Animal.clearZoo();
-    Animal.createAnimal(specie.value, tail.value, color.value, hasHorns.checked);
-    Animal.renderZoo();
-})
-
-//14. sukurti metodą kuris generuotų atsitiktinius skaičius nuo 1000000 iki 9999999 ir priskirti tą skaičių savybei id, ir pasileistų konstruktoriuje
+Animal.start();
